01a38d55616f9e14c42d733a11df138a46755b43
David <davidjf311@gmail.com>
2019-10-08 21:02:35 -0600

    Upload of working version of my social card/instagram copy

--- socialcard/src/App.js
+++ socialcard/src/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+//import React from 'react';
+//import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Post from './components/Post';
 
-function App() {
+class App extends Component {
+      render() {
+        return (
+          <div className="App">
+            <Header />
+            <section className="App-main">
+            <Post 
+
+            nickname="David" 
+            avatar={require('./components/Post/unite.jpg')}
+            caption="Not a bad view" 
+            image={require('./components/Post/nature.jpg')}/>
+
+            </section>
+            </div>
+        );
+      }
+    }
+export default App;
+
+
+
+
+
+
+
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +53,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
--- socialcard/src/index.js
+++ socialcard/src/index.js
@@ -11,46 +11,6 @@ import * as serviceWorker from './serviceWorker';
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
 
-/**function Card(props)
-{
-	return(
-		<div className="card">
-			<img scr={props.featureImage} alt="image"
-				src = "https://via.placeholder.com/600x250.png"
-				alt = "cap_image" />
-		<div>
-			<h5 >{props.title}</h5>
-			<p >{props.description}</p>
-			<a href={props.link} >Learn more</a>
-		</div>
-		</div>
-		);
-}
 
-function CardList()
-{
-	return(
-		<div >
-			<div >
-				<Card 	featureImage="https://sebhastian.com/static/eb0e936c0ef42ded5c6b8140ece37d3e/fcc29/feature-image.png"
-					title="how to make an interactive reactjs form"
-					description="let's write soem interactive form with react"
-					link="https://sebhastian.som/interactive-react-form"
-					/>
-			</div>
-			<div >
-				<Card />
-			</div>
-			<div >
-				<Card />
-			</div>
-		);
-}**/
-
-function CardList()
-{
-	return "hello world";
-}
-
-ReactDOM.render(<CardList />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
